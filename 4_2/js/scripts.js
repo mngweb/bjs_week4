93529bbc7dc9bbe9217654c8b241b37e244a1120
mngweb <mangoweb@wp.pl>
2017-05-12 19:51:11 +0200

    Tydzien 4

--- 4_2/js/scripts.js
+++ 4_2/js/scripts.js
@@ -1,7 +1,3 @@
-
-
-//// WERSJA 1
-
 (function($) {
 
     $(document).ready(function() {
@@ -18,66 +14,19 @@
 
 
             nav.stop().slideToggle(500).transition({
-                //opacity: "toggle",       // PYTANIE 1: czy "opacity: 'toggle'" nie działa z biblioteką jquery.transit? 
                 opacity: !isShowed ? 1 : 0,
-                left: !isShowed ? 0 : -navWidth
+                x: !isShowed ? 0 : -navWidth
             }, 
             {
-                duration: 500,
-                //easing: "easeOutBounce",      // PYTANIE 2: Dlaczego nie działa (nie widzę takiego działania animacji jak na stronie biblioteki http://gsgd.co.uk/sandbox/jquery/easing/ jest pokazana) mimo podpięcia biblioteki?
+                duration: 1000,
+                easing: 'cubic-bezier(.83,.27,.41,1.7)',  
                 queue: false  
             }); 
 
             isShowed = !isShowed;
-
 
         });
 
     });
 
 })(jQuery);
-
-
-
-
-
-
-
-//// WERSJA 2 ( z jednym PYTANIEM ) - proszę całość powyżej zakomentować a całość poniżej odkomentować
-
-// (function($) {
-
-//     $(document).ready(function() {
-
-//         var nav = $(".nav-main");
-
-
-//         $(".toggle-menu").on("click", function() {
-
-            
-//             //PYTANIE: Jeśli kliknę na hamburgera -> to nawigacja rozwija się - i szybko gdy ona się pojawi kliknę ponownie jeden raz hamburger, to powiedzmy mniej więcej w połowie wizualnego efektu rozwinięcia prawidłowo animacja robi stop() i zwija z powrotem nawigację. Czyli chyba dobrze rozpoznaje w trakcie animacji to ":visible". Ale gdy poklikam wiele razy szybko w hamburgera, to coś się blokuje - powstaje jakaś kolejka? - nie bardzo rozumiem co się wtedy dzieje skoro przy pojedynczym kliknięciu w trakcie animacji prawidłowo rozpoznawało widoczność?
-
-//             var isVisible = nav.is(":visible"),
-//                 navWidth = nav.outerWidth(); 
-
-//             $(this).toggleClass('click', !isVisible);
-
-
-//             nav.stop().slideToggle(500).transition({
-//                 opacity: !isVisible ? 1 : 0,
-//                 left: !isVisible ? 0 : -navWidth
-//             }, 
-//             {
-//                 duration: 500,
-//                 queue: false  
-//             }); 
-
-
-
-//         });
-
-//     });
-
-// })(jQuery);
-
-
